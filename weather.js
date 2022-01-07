2694cd2df9030b68952ef58facf7bada44decc0e
rxthew <richardtheumaster@gmail.com>
2022-01-07 17:38:31 +0100

    curated weather data & conversion to DOM

--- weather.js
+++ weather.js
@@ -7,23 +7,34 @@ const weatherApp = (function(){
         //Array of labels to be used, and object keys from weatherData to return values we need//
         const labelsAndTargets = 
         [
-            ['Date', 'dt_txt']
-            //populate more.
+            ['Date', 'dt_txt'],
+            ['Forecast', 'description'],
+            ['Temperature', 'temp'],
+            ['Humidity', 'humidity'],
+            ['Feels like', 'feels_like'],
+
+
         ];
 
         const parseIndividualValue = function(someLabelsAndTargets,someFinalArray,weatherListElem){
-            for (let elemArr of someLabelsAndTargets){
+            for (let someLabelAndTarget of someLabelsAndTargets){
+    
                 const weatherObj = {};
-                weatherObj[elemArr[0]] = weatherListElem[elemArr[1]]
-                someFinalArray.push(weatherObj) 
-                return weatherObj
+                
+                if(weatherListElem.hasOwnProperty(someLabelAndTarget[1])){
+                    weatherObj[someLabelAndTarget[0]] = weatherListElem[someLabelAndTarget[1]];
+                    someFinalArray.push(weatherObj); 
+                }
+                
             }
         }
         
         const parseAllValues = function(){
             const finalArray = []
             for (let elem of weatherData.list){
-                parseIndividualValue(labelsAndTargets, finalArray, elem)
+                parseIndividualValue(labelsAndTargets, finalArray, elem);
+                parseIndividualValue(labelsAndTargets, finalArray, elem.main);
+                parseIndividualValue(labelsAndTargets, finalArray, elem.weather[0])
             }
             return finalArray; 
         }    
@@ -31,13 +42,36 @@ const weatherApp = (function(){
         
         return parseAllValues()    
         
-        }     
+        }
+        
+    const weatherObjectToDOM = function(finalArray){
+
+        const weatherContainer = document.querySelector('#weatherContainer');
+
+        const convertorFunction = (function(){
+            for (let obj of finalArray){
+                let component = document.createElement('span');
+                let text = document.createElement('p');
+                text.textContent = (function(){
+                    for (let [key,value] of Object.entries(obj)){
+                        return `${key} : ${value}`
+                    }  
+                })()
+                component.appendChild(text)
+                weatherContainer.appendChild(component)
+            }
+        })()
+
+
+        return
+    }    
     
     const weatherData = function(){
                          let city = document.querySelector('#cityInput').value;
                          fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${key}`, {mode:'cors'})
                         .then(function(response){return response.json()})
-                        .then(function(info){console.log(parseWeatherData(info))})
+                        .then(function(info){return parseWeatherData(info)})
+                        .then(function(weatherObj){return weatherObjectToDOM(weatherObj)})
     }
     submit.addEventListener('click', weatherData);
 })()
